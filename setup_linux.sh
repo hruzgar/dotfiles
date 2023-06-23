1ef2f7aed277b8be6d97a87a5319a57ba11f3f1d
Hasan Rüzgar <84530266+hruzgar@users.noreply.github.com>
2023-06-23 23:53:26 +0200

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd
 
 sudo apt update -y
 sudo apt upgrade -y
@@ -6,7 +7,9 @@ sudo apt upgrade -y
 sudo apt install zsh ranger exa neovim -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/jeffreytse/zsh-vi-mode $HOME/.oh-my-zsh/custom/plugins/zsh-vi-mode
+
 git clone https://github.com/hruzgar/dotfiles.git
 cp -f dotfiles/.zshrc .
 cp -f dotfiles/.p10k.zsh .
+
+git clone https://github.com/jeffreytse/zsh-vi-mode.git $HOME/.oh-my-zsh/custom/plugins/zsh-vi-mode
