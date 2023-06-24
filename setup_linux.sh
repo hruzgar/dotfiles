f8e50bf76a3edcb449e1c889b56128aeace8f6a1
Hasan Rüzgar <84530266+hruzgar@users.noreply.github.com>
2023-06-24 13:11:54 +0200

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 MYUSER=$USER
 
-cd
+# change to home dir
+cd ~
 
+# update package repository
 sudo apt update -y
 sudo apt upgrade -y
 
+# install apps
 sudo apt install zsh ranger exa neovim -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# get custom config files
 git clone https://github.com/hruzgar/dotfiles.git
 cp -f dotfiles/.zshrc .
 cp -f dotfiles/.p10k.zsh .
 
-git clone https://github.com/jeffreytse/zsh-vi-mode.git $HOME/.oh-my-zsh/custom/plugins/zsh-vi-mode
-
+# change default shell to zsh
 sudo chsh -s /bin/zsh $MYUSER
-echo "Please restart your shell or start a new session to use Zsh."
 
-#source ~/.zshrc
-#exec zsh
-
+echo "\nPlease restart your shell or start a new session to use Zsh.\n"
